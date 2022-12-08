1fc29861e3136fdd958a298d70c875f2361b3bc2
Subhash Bhandari <106956738+Science-Enthusiasts@users.noreply.github.com>
2022-12-08 19:49:20 +0545

    Create hand_detection.py

---
+++ hand_detection.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp 
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+while True:
+	success, img = cap.read()
+	imgRBG = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	results = hands.process(imgRBG)
+	print(results.multi_hand_landmarks)
+
+	if results.multi_hand_landmarks:
+		for handLms in results.multi_hand_landmarks:
+			mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+	cv2.imshow("Image",img)
+	cv2.waitKey(1)
